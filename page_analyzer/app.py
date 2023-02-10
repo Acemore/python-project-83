8f81cf099117fd72197f48c8178d1846479b4222
Sergey Martynov <acemore@mail.ru>
2023-02-10 10:55:18 +0300

    Update main page handler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
 
-@app.get('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
