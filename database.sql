1d5cae083298bbc19e4d68f198f72ac4ed67914d
Sergey Martynov <acemore@mail.ru>
2023-02-14 01:38:57 +0300

    Add table 'url_checks'to project db

--- database.sql
+++ database.sql
@@ -1,5 +1,15 @@
 CREATE TABLE urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name varchar(255) UNIQUE NOT NULL,
+    created_at date DEFAULT NOW()
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 text,
+    title text,
+    description text,
     created_at date DEFAULT NOW()
 );
