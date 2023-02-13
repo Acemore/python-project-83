b1dbe51efce0b2a5c5d2aebd38ff595e42992bf8
Sergey Martynov <acemore@mail.ru>
2023-02-13 11:30:21 +0300

    Add urls info getting via db

---
+++ page_analyzer/db.py
@@ -0,0 +1,34 @@
+from flask import flash
+from psycopg2.extras import NamedTupleCursor
+
+
+def get_url_by_id(conn, url_id):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
+        url = curs.fetchone()
+
+    return url
+
+
+def get_url_id_by_url_name(conn, url_name):
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('SELECT * FROM urls WHERE name = %s;', (url_name,))
+        url_in_list_to_check = curs.fetchone()
+
+        if url_in_list_to_check:
+            flash('Страница уже существует', 'info')
+            id = url_in_list_to_check.id
+        else:
+            curs.execute('INSERT INTO urls (name) VALUES (%s) RETURNING id', (url_name,))
+            id, = curs.fetchone()
+            flash('Страница успешно добавлена', 'success')
+
+    return id
+
+
+def get_urls(conn):
+    with conn.cursor() as curs:
+        curs.execute('SELECT * FROM urls ORDER BY id DESC;')
+        urls = curs.fetchall()
+
+    return urls
