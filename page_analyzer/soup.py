a53c929fa3351ebab83378ef6254701c94436608
Sergey Martynov <acemore@mail.ru>
2023-03-10 17:54:29 +0300

    Update soup.py

--- page_analyzer/soup.py
+++ page_analyzer/soup.py
@@ -6,16 +6,26 @@ def get_tags_data(url_name):
     resp = requests.get(url_name)
     soup = BeautifulSoup(resp.text, 'html.parser')
 
-    h1 = h1_tag.text if (h1_tag := soup.h1) else ''
-    title = title_tag.text if (title_tag := soup.title) else ''
-    description = meta_tag.get('content') if (
-        meta_tag := soup.find('meta', attrs={'name': 'description'})
-    ) else ''
+    if (h1_tag := soup.h1):
+        h1 = h1_tag.text
+    else:
+        h1 = ''
+    # h1 = h1_tag.text if (h1_tag := soup.h1) else ''
+    if (title_tag := soup.title):
+        title = title_tag.text
+    else:
+        title = ''
+    # title = title_tag.text if (title_tag := soup.title) else ''
+    if (meta_tag := soup.find('meta', attrs={'name': 'description'})):
+        description = meta_tag.get('content')
+    else:
+        description = ''
+    # description = meta_tag.get('content') if (
+        # meta_tag := soup.find('meta', attrs={'name': 'description'})
+    # ) else ''
 
-    tags_data = {
+    return {
         'h1': h1,
         'title': title,
         'description': description,
     }
-
-    return tags_data
