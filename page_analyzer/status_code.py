aec23bc035bc1b9bec0f362dc27570b689ba5814
Sergey Martynov <acemore@mail.ru>
2023-03-10 17:48:54 +0300

    Move status code and urls features to web_utils

--- page_analyzer/status_code.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_status_code_by_url_name(url_name):
-    try:
-        return requests.get(url_name).status_code
-    except requests.RequestException:
-        return 404
--- page_analyzer/web_utils.py
+++ page_analyzer/web_utils.py
@@ -1,10 +1,23 @@
+import requests
+from flask import redirect, url_for
 from urllib.parse import urlparse
 from validators.url import url as is_correct_url
 
 
-def normalize_url(url):
+def get_main_page_url(url):
     parsed_url = urlparse(url)
     return f'{parsed_url.scheme}://{parsed_url.netloc}'
+
+
+def get_redirect(handler_name, id):
+    return redirect(url_for(handler_name, id=id))
+
+
+def get_status_code_by_url_name(url_name):
+    try:
+        return requests.get(url_name).status_code
+    except requests.RequestException:
+        return 0
 
 
 def validate(url):
