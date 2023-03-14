c056fb82761e758e4ffdcaca5c00639022224fc8
Sergey Martynov <acemore@mail.ru>
2023-03-14 13:44:41 +0300

    Update module 'soup'

--- page_analyzer/soup.py
+++ page_analyzer/soup.py
@@ -3,26 +3,25 @@ from bs4 import BeautifulSoup
 
 
 def get_tags_data(url_name):
+    h1 = ''
+    title = ''
+    description = ''
+
     resp = requests.get(url_name)
     soup = BeautifulSoup(resp.text, 'html.parser')
 
     h1_tag = soup.h1
+    title_tag = soup.title
+    meta_tag = soup.find('meta', attrs={'name': 'description'})
+
     if h1_tag:
         h1 = h1_tag.text
-    else:
-        h1 = ''
-    # h1 = h1_tag.text if (h1_tag := soup.h1) else ''
-    title_tag = soup.title
     if title_tag:
         title = title_tag.text
-    else:
-        title = ''
-    # title = title_tag.text if (title_tag := soup.title) else ''
-    meta_tag = soup.find('meta', attrs={'name': 'description'})
     if meta_tag:
         description = meta_tag.get('content')
-    else:
-        description = ''
+    # h1 = h1_tag.text if (h1_tag := soup.h1) else ''
+    # title = title_tag.text if (title_tag := soup.title) else ''
     # description = meta_tag.get('content') if (
         # meta_tag := soup.find('meta', attrs={'name': 'description'})
     # ) else ''
