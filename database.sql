14d36ff1c693aaff321375fbb0b3d301bf356bf3
Acemore <91070379+Acemore@users.noreply.github.com>
2024-12-07 19:06:42 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name varchar(255) UNIQUE NOT NULL,
     created_at date
 );
 
+DROP TABLE IF EXISTS url_checks;
 CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
     url_id bigint REFERENCES urls (id),
